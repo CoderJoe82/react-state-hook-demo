8cd4d62ca702366649fa7e39603e8611345b2b04
Joseph Padgett <coderjoe82@gmail.com>
2025-12-11 09:25:25 -0500

    Enhance App.tsx with explanatory comments
    Added comments to explain state hook and menu items logic.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import ButtonDisplay from './components/ButtonDisplay'
 
 function App() {
   const [menuSelection, setMenuSelection] = useState("family")
+  /*
+  This is the state hook. It's essentially the same thing as saying this:
+  const menuSelection = "family"
+  Except, normally you couln't change what's in a const. the state hook uses the second variable name as a function call to say whatever
+  we put in this function call (setMenuSelection) is what menuSelection will become.
+  */
 
   const familyItems = [
     { label: "Family doctor", href: "/family" },
@@ -15,13 +21,21 @@ function App() {
     { label: "Personal doctor", href: "/personal" },
     { label: "Facilities", href: "/facilities" }
   ]
+  /*
+  These two above are pretty self explanatory. They're holding what your menu items are.
+  */
 
   const currentItems = menuSelection === "family" ? familyItems : individualItems
+  /*
+  This is a short hand if statement to say if menuSelection is currently set as "family" than currentItems is set to the familyItems array.
+  Else, set currentItems to individualItems array
+  */
 
   return (
     <div>
       <ButtonDisplay handleSelection={setMenuSelection} />
       <MenuBar items={currentItems} />
+      {/*These are my two components that we're passing the function from the state hook to, as well as the currentItems to.*/}
     </div>
   )
 }
