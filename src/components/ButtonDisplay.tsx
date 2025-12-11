274520529c2ed0649365738880acb380890057c1
Joseph Padgett <coderjoe82@gmail.com>
2025-12-11 09:35:38 -0500

    Enhance ButtonDisplay with detailed comments
    Added comments to explain the ButtonDisplay component and its props.

--- src/components/ButtonDisplay.tsx
+++ src/components/ButtonDisplay.tsx
@@ -1,8 +1,21 @@
 interface ButtonDisplayProps {
     handleSelection: (selection: string) => void;
 }
+/*
+React with vanilla javascript handles dynamic components with just the keyword props. With typescript, you handle it like this.
+handleSelection is just what you're calling your 'function' to happen.
+The word 'selection' here could be anything.
+String here is telling the prop what type of thing is required to be expected.
+The => void part is there because you have to include that to tell typescript what you're expected to have returned, and what type of data
+will be returned. In this case, we don't actually need anything returned when you click something, or make your 'selection', so we use
+void, which is the 'blank' keyword to say nothing to return.
+*/
 
 function ButtonDisplay({ handleSelection }: ButtonDisplayProps) {
+    /*
+    Here, you're destructuring the ButtonDisplayProps object to access the function directly so you don't have to do props.handleSelection.
+    And ButtonDisplayProps is required there so it knows what type of thing we're using inside. Here, a function.
+    */
     return (
         <>
             <h1>
@@ -10,6 +23,10 @@ function ButtonDisplay({ handleSelection }: ButtonDisplayProps) {
             </h1>
             <button onClick={() => handleSelection("family")}>Family</button>
             <button onClick={() => handleSelection("individual")}>Individual</button>
+            {/*
+            This is just us saying handleSelection is meant to be a placeholder word for whatever function we pass into this component
+            wherever we call it, like an argument or parameter in a basic javascript function.
+            */}
         </>
     )
 }
